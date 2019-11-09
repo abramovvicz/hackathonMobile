dd825d02a04707be3b744a1458a9280b0996e52b
Miki <rupniewskimikolaj@gmail.com>
2019-11-09 01:08:04 +0100

    account fragment changed

--- app/src/main/java/com/harman/borsuki/carpooling/ui/account/AccountViewModel.java
+++ app/src/main/java/com/harman/borsuki/carpooling/ui/account/AccountViewModel.java
@@ -6,14 +6,4 @@ import androidx.lifecycle.ViewModel;
 
 public class AccountViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
-
-    public AccountViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is account fragment");
-    }
-
-    public LiveData<String> getText() {
-        return mText;
-    }
 }
